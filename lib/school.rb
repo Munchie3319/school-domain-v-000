d1e97c019c8b45a755e6ae5f512cb45ebb8b324c
rosa lee <rosalee0116@gmail.com>
2019-12-13 20:35:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -6,5 +6,9 @@ school.roster
 school.add student("Zach Morris", 9)
 school.roster
 
-hash ["new key"]
+hash ["new key"] = []
+hash ["new_key"] <<
 "new_value_for_value_array"
+
+Hash
+=> {"new_key" => ["new_value_for_value_array"]}
